68b5d60814ad0bd82290629f504e892e048f081a
Melissa <melissa.rios.sousa@gmail.com>
2023-09-07 11:00:41 -0300

    Criação de novos cenários

--- e2e/3-registro-de-usuario/registro-de-usuario.cy.js
+++ e2e/3-registro-de-usuario/registro-de-usuario.cy.js
@@ -17,7 +17,7 @@ describe('Cenário Registro de Usuários', () => {
         cy.get('input[id="customer.address.zipCode"]').type('111111');
         cy.get('input[id="customer.phoneNumber"]').type('22222222');
         cy.get('input[id="customer.ssn"]').type('1111112222');
-        cy.get('input[id="customer.username"]').type(generateFirstName());
+        cy.get('input[id="customer.username"]').type('bsinho12'); //.type(generateFirstName())
         cy.get('input[id="customer.password"]').type('bsobso21');
         cy.get('input[id="repeatedPassword"]').type('bsobso21');
 
@@ -25,5 +25,54 @@ describe('Cenário Registro de Usuários', () => {
         cy.get('#rightPanel').contains('Your account was created successfully. You are now logged in.')
       })
   
-     
+      it('Validar Registro de Usuário com Username já existente', () => {
+        cy.get('input[id="customer.firstName"]').type('Bergson');
+        cy.get('input[id="customer.lastName"]').type('Miau');
+        cy.get('input[id="customer.address.street"]').type('Casa do Bso');
+        cy.get('input[id="customer.address.city"]').type('Fortal');
+        cy.get('input[id="customer.address.state"]').type('CE');
+        cy.get('input[id="customer.address.zipCode"]').type('111111');
+        cy.get('input[id="customer.phoneNumber"]').type('22222222');
+        cy.get('input[id="customer.ssn"]').type('1111112222');
+        cy.get('input[id="customer.username"]').type('bsinho12'); //cenário funciona somente se já tiver feito o registro anteriormente
+        cy.get('input[id="customer.password"]').type('bsobso21');
+        cy.get('input[id="repeatedPassword"]').type('bsobso21');
+
+        cy.get('input[class="button"]').contains('Register').click();
+        cy.get('span[id="customer.username.errors"]').contains('This username already exists.');
+      })
+
+      it('Validar Registro de Usuário sem First Name', () => {
+        cy.get('input[id="customer.lastName"]').type('Miau');
+        cy.get('input[id="customer.address.street"]').type('Casa do Bso');
+        cy.get('input[id="customer.address.city"]').type('Fortal');
+        cy.get('input[id="customer.address.state"]').type('CE');
+        cy.get('input[id="customer.address.zipCode"]').type('111111');
+        cy.get('input[id="customer.phoneNumber"]').type('22222222');
+        cy.get('input[id="customer.ssn"]').type('1111112222');
+        cy.get('input[id="customer.username"]').type(generateFirstName()); 
+        cy.get('input[id="customer.password"]').type('bsobso21');
+        cy.get('input[id="repeatedPassword"]').type('bsobso21');
+
+        cy.get('input[class="button"]').contains('Register').click();
+        cy.get('span[id="customer.firstName.errors"]').contains('First name is required.')
+      })
+
+      it('Validar Registro de Usuário com espaço no First Name', () => {
+        cy.get('input[id="customer.firstName"]').type(' ');
+        cy.get('input[id="customer.lastName"]').type('Miau');
+        cy.get('input[id="customer.address.street"]').type('Casa do Bso');
+        cy.get('input[id="customer.address.city"]').type('Fortal');
+        cy.get('input[id="customer.address.state"]').type('CE');
+        cy.get('input[id="customer.address.zipCode"]').type('111111');
+        cy.get('input[id="customer.phoneNumber"]').type('22222222');
+        cy.get('input[id="customer.ssn"]').type('1111112222');
+        cy.get('input[id="customer.username"]').type(generateFirstName());
+        cy.get('input[id="customer.password"]').type('bsobso21');
+        cy.get('input[id="repeatedPassword"]').type('bsobso21');
+
+        cy.get('input[class="button"]').contains('Register').click();
+        cy.get('span[id="customer.firstName.errors"]').contains('First name is required.') //irá falhar pois há falha que permite esse cenário.
+      })
+
     })
